25f9382a083a1ad0a0e3a92a93329cf811877ba8
justdheja <justdheja@gmail.com>
2020-02-21 23:41:30 +0700

    'init'

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: IdCard(),
+));
+
+class IdCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[350],
+      appBar: AppBar(
+        title: Text(
+          'ID Card',
+          style: TextStyle(
+            color: Colors.red[900], 
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[350],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/profile.jpg'),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 40.0,
+              color: Colors.grey[400],
+            ),
+            Text(
+              'Nama',
+              style: TextStyle(
+                color: Colors.grey[900],
+                letterSpacing: 2.0
+              )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Dheja',
+              style: TextStyle(
+                fontSize: 28.0,
+                color: Colors.red[900],
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'Age',
+              style: TextStyle(
+                color: Colors.grey[900],
+                letterSpacing: 2.0
+              )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '20',
+              style: TextStyle(
+                fontSize: 28.0,
+                color: Colors.red[900],
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.red[900]
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'justdheja@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[900],
+                    fontSize: 18.0,
+                    letterSpacing: 2.0
+                  ),
+                )
+              ]
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.public,
+                  color: Colors.red[900]
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'justdheja.rf.gd',
+                  style: TextStyle(
+                    color: Colors.grey[900],
+                    fontSize: 18.0,
+                    letterSpacing: 2.0
+                  ),
+                )
+              ]
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
